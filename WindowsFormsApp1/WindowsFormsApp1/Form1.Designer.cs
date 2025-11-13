28b41dc8e3cf3bd92de085d6dc36f6137e1ad352
kernel <144273450+KERNELEET@users.noreply.github.com>
2025-11-13 07:40:52 -0800

    code pushed

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LstCommands = new System.Windows.Forms.ListBox();
+            this.TmrSpeaking = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LstCommands
+            // 
+            this.LstCommands.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.LstCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LstCommands.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LstCommands.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.LstCommands.FormattingEnabled = true;
+            this.LstCommands.ItemHeight = 18;
+            this.LstCommands.Location = new System.Drawing.Point(0, 0);
+            this.LstCommands.Name = "LstCommands";
+            this.LstCommands.Size = new System.Drawing.Size(524, 425);
+            this.LstCommands.TabIndex = 0;
+            this.LstCommands.SelectedIndexChanged += new System.EventHandler(this.LstCommands_SelectedIndexChanged);
+            // 
+            // TmrSpeaking
+            // 
+            this.TmrSpeaking.Enabled = true;
+            this.TmrSpeaking.Interval = 1000;
+            this.TmrSpeaking.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources._4ff0650a75cd3cfaf6c6b713391588ab;
+            this.pictureBox1.Location = new System.Drawing.Point(124, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(314, 251);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(524, 425);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LstCommands);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstCommands;
+        private System.Windows.Forms.Timer TmrSpeaking;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Sarah = new SpeechSynthesizer();
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int RecTimeOut = 0;
+        DateTime TimeNow = DateTime.Now;
+        string currentOperation = "";
+        int step = 0;
+        double num1, num2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(_recognizer_SpeechDetected);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startlistening_SpeechRecognized);
+        }
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if (speech == "Hello")
+            {
+                Sarah.SpeakAsync("Hello, I am here");
+            }
+            else if (speech == "How are you")
+            {
+                Sarah.SpeakAsync("I am working normally");
+            }
+            else if (speech == "What date is it today")
+            {
+                Sarah.SpeakAsync(DateTime.Now.ToString("MMMM dd, yyyy"));
+            }
+            else if (speech == "What Islamic date is it today")
+            {
+                HijriCalendar hijri = new HijriCalendar();
+                DateTime now = DateTime.Now;
+                string hijriDate = string.Format("{0} {1} {2}",
+                    hijri.GetDayOfMonth(now),
+                    hijri.GetMonth(now),
+                    hijri.GetYear(now));
+                Sarah.SpeakAsync($"The Hijri date today is {hijriDate}");
+            }
+            else if (speech == "What time is it")
+            {
+                Sarah.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+            else if (speech == "Math")
+            {
+                Sarah.SpeakAsync("Which operation would you like to perform? Say 1 for Addition, 2 for Subtraction, 3 for Multiplication, or 4 for Division.");
+                step = 1;
+            }
+            else if (step == 1)
+            {
+                if (speech == "1" || speech == "2" || speech == "3" || speech == "4")
+                {
+                    switch (speech)
+                    {
+                        case "1":
+                            currentOperation = "Addition";
+                            break;
+                        case "2":
+                            currentOperation = "Subtraction";
+                            break;
+                        case "3":
+                            currentOperation = "Multiplication";
+                            break;
+                        case "4":
+                            currentOperation = "Division";
+                            break;
+                    }
+                    Sarah.SpeakAsync("Please provide the first number.");
+                    step = 2;
+                }
+                else
+                {
+                    Sarah.SpeakAsync("Please choose a valid operation: Say 1 for Addition, 2 for Subtraction, 3 for Multiplication, or 4 for Division.");
+                }
+            }
+            else if (step == 2)
+            {
+                if (double.TryParse(speech, out num1))
+                {
+                    Sarah.SpeakAsync("Please provide the second number.");
+                    step = 3;
+                }
+                else
+                {
+                    Sarah.SpeakAsync("I couldn't understand the number. Please provide the first number.");
+                }
+            }
+            else if (step == 3)
+            {
+                if (double.TryParse(speech, out num2))
+                {
+                    PerformCalculation();
+                    step = 0;
+                }
+                else
+                {
+                    Sarah.SpeakAsync("I couldn't understand the number. Please provide the second number.");
+                }
+            }
+            else if (speech == "Stop talking")
+            {
+                Sarah.SpeakAsyncCancelAll();
+                ranNum = rnd.Next(1, 2);
+                if (ranNum == 1)
+                {
+                    Sarah.SpeakAsync("Yes Sir");
+                }
+                if (ranNum == 2)
+                {
+                    Sarah.SpeakAsync("I'm sorry, I will be quiet");
+                }
+            }
+            else if (speech == "Stop listening")
+            {
+                Sarah.SpeakAsync("If you need me, just ask");
+                _recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else if (speech == "Show commands")
+            {
+                string[] commands = (File.ReadAllLines(@"DefaultCommands.txt"));
+                LstCommands.Items.Clear();
+                LstCommands.SelectionMode = SelectionMode.None;
+                LstCommands.Visible = true;
+                foreach (string command in commands)
+                {
+                    LstCommands.Items.Add(command);
+                }
+            }
+            else if (speech == "Hide commands")
+            {
+                LstCommands.Visible = false;
+            }
+            else if (speech == "Goodbye Sarah")
+            {
+                Sarah.SpeakAsync("Goodbye. Have a great day!");
+                Application.Exit();
+            }
+            else if (speech == "Open YouTube")
+            {
+                OpenWebsite("https://www.youtube.com");
+                Sarah.SpeakAsync("Opening YouTube.");
+            }
+        }
+
+        private void PerformCalculation()
+        {
+            double result = 0;
+            switch (currentOperation)
+            {
+                case "Addition":
+                    result = num1 + num2;
+                    break;
+                case "Subtraction":
+                    result = num1 - num2;
+                    break;
+                case "Multiplication":
+                    result = num1 * num2;
+                    break;
+                case "Division":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Sarah.SpeakAsync("Cannot divide by zero.");
+                        return;
+                    }
+                    break;
+            }
+            Sarah.SpeakAsync($"The result of {currentOperation} is {result}");
+        }
+
+        private void _recognizer_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+            if (speech == "Wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                Sarah.SpeakAsync("Yes, I'm here. Hello again.");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void LstCommands_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void OpenWebsite(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch (Exception ex)
+            {
+                Sarah.SpeakAsync("Sorry, I couldn't open the website.");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10)
+            {
+                _recognizer.RecognizeAsyncCancel();
+            }
+            else if (RecTimeOut == 11)
+            {
+                TmrSpeaking.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+    }
+}
